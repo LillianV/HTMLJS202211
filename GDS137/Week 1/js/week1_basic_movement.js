86a9e42af671f00bd37abcf84b9455b9c14c2ad1
LillianV <lvasseur@email.neit.edu>
2023-03-31 14:27:13 -0400

    Ball

--- GDS137/Week 1/js/week1_basic_movement.js
+++ GDS137/Week 1/js/week1_basic_movement.js
@@ -13,6 +13,12 @@ var player;
 	
 	//Instantiate the Player
 	player = new Player();
+	player.vx = -10;
+	player.vy = -10;
+	player.color = "purple";
+	player.width = 40;
+	player.height = 40;
+		
 	
 	//Set the Animation Timer
 	timer = setInterval(animate, interval);
@@ -23,8 +29,36 @@ function animate()
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
 	//Move the Player
-	player.x += 2;
+	player.move();
+
+	//collision
+	if(player.x < player.width/2){
+		player.x = player.width/2
+		player.vx = -player.vx;
+	}
+	
+	if(player.x > canvas.width - player.width/2){
+		player.x = canvas.width - player.width/2
+		player.vx = -player.vx;
+	}
+	if(player.y < player.height/2){
+		player.y = player.height/2
+		player.vy = -player.vy;
+	}
+	
+	if(player.y > canvas.height - player.height/2){
+		player.y = canvas.height - player.height/2
+		player.vy = -player.vy;
+	}
 	
 	//Update the Screen
 	player.draw();
 }
+
+
+
+
+
+
+// code notes for pong, if(ball.y < p1.y + p1.height/6)
+// ball only hits one side of the paddles, make the paddles tall and skinny, ball must be small
