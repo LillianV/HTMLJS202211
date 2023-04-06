5664eb88ab105b153c507a3bcb9ebfef73a61cf9
LillianV <lvasseur@email.neit.edu>
2023-04-06 11:51:12 -0400

    make box

--- GDS137/Week 2/js/GameObject.js
+++ GDS137/Week 2/js/GameObject.js
@@ -4,7 +4,7 @@ function GameObject(x,y,w,h,color)
 	
 	//Default Values
 	if(x == undefined)
-		this.x = canvas.width/2;
+		this.x = 100;
 	else 
 		this.x = x;
 	if(y == undefined)
@@ -13,11 +13,11 @@ function GameObject(x,y,w,h,color)
 		this.y = y;
 	
 	if(w == undefined)
-		this.width = 100;
+		this.width = 25;
 	else 
 		this.width = w;
 	if(h == undefined)
-		this.height = 100;
+		this.height = 200;
 	else 
 		this.height = h;
 	
