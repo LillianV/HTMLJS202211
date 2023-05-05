c392b3c5e742ac134dfb70c03aa534841f9807f6
LillianV <lvasseur@email.neit.edu>
2023-05-05 13:49:34 -0400

    switching sides

--- GDS137/level4BigBoss/js/basic_platformer.js
+++ GDS137/level4BigBoss/js/basic_platformer.js
@@ -9,10 +9,13 @@ var follower;
 var following = false;
 
 
+
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 
 	player = new GameObject({x:100, y:canvas.height/2-100});
+
+	var followPoint = player.left();
 
 	follower = new GameObject();
 	follower.y = canvas.height/2;
@@ -63,10 +66,12 @@ function animate()
 	if(a)
 	{
 		player.vx += -player.ax * player.force;
+		followPoint = player.right();
 	}
 	if(d)
 	{
 		player.vx += player.ax * player.force;
+		followPoint = player.left();
 	}
 
 	player.vx *= fX;
@@ -177,15 +182,15 @@ function animate()
 
 function follow() //actually magnet function
 {
-	var dx = player.x - follower.x + 110;
-	var dy = player.y - follower.y;
-	
-	//Not using this in this function but...
-	//This is the Pythagorean Theorem and gets the hypoteneuse of a triangle.
-	//This can be used to get the actual distance between two points.
-	var dist = Math.sqrt(dx * dx + dy * dy);
+	var dx = followPoint.x - follower.x;
+	var dy = followPoint.y - follower.y;
 	
 	follower.x += dx /25;
 	follower.y += dy /25;
 }
+
+
+
+
+
 
