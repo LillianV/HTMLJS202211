f51961bfb0aab48f3a523db89c3fcc9514223c62
LillianV <lvasseur@email.neit.edu>
2023-05-19 13:17:39 -0400

    level 6 big boss

--- GDS137/Basic Game Setup/js/game.js
+++ GDS137/Basic Game Setup/js/game.js
@@ -7,25 +7,46 @@ var timer = setInterval(animate, interval);
 //player = new GameObject({x:150});
 var frictionX = .85;	
 var frictionY = 1;
-var gravity = 1;
+var gravity = 4;
 var player;
 var ball;
-var enemy;
+var square;
+var p1Score = 0;
 
 player = new GameObject();
-	player.force = 0.5;
+	player.force = 1;
 	player.height= 50;
 	player.width = 50;
 	player.x = canvas.width/2;
 	player.y = 775;
 	player.color = "#ffff00";
 
+	var amt = 5;	
+	var ball = [];
+	var square = [];
+	
+	for(var i = 0; i < amt; i++)
+	{
+		ball[i] = new GameObject();
+		ball[i].x = Math.random() * (100 - 0) + 0;
+		ball[i].y = Math.random() * (800 - 0) + 0;
+		ball[i].width = 20;
+		ball[i].height = dots[i].width;
+		ball[i].vy = 2;
+		ball[i].color = "red";
+	}
 
-	ball = new GameObject();
-	ball.force = 2;
-	ball.width = 20;
-	ball.height = 20;
-	ball.color = "magenta";
+	for(var i = 0; i < amt; i++)
+	{
+		square[i] = new GameObject();
+		square[i].x = Math.random() * (100 - 0) + 0;
+		square[i].y = Math.random() * (800 - 0) + 0;
+		square[i].width = 20;
+		square[i].height = dots[i].width;
+		square[i].vy = 2;
+		square[i].color = "green";
+	}
+
 //-------------------------AnimationLoop--------------------------------
 
 function animate()
@@ -44,9 +65,15 @@ function animate()
 	player.vx *= frictionX;
 	player.x += player.vx;
 
-
-
-
+	//Ball and square gravity
+	
+	ball[i].vy += gravity;
+	
+	ball[i].y = ball[i].vy;
+	
+	square[i].vy += gravity;
+	
+	square[i].y = square[i].vy;
 
 //right canvas collision
 if(player.x > canvas.width - player.width/2)
@@ -63,16 +90,39 @@ if(player.x < player.width/2)
 		player.vx = 0;
 	}
 
+	if (ball[i].hitTestObject(player)) {
+		player.x--;
+		player.vx = 0;
+		ball[i].y = Math.random() * (100 - 0) + 0;
+		ball[i].x = Math.random() * (800 - 0) + 0;
+		console.log("colliding");
+		p1Score = 0;
+	}
+	if (square[i].hitTestObject(player)) {
+		player.x--;
+		player.vx = 0;
+		square[i].y = Math.random() * (100 - 0) + 0;
+		square[i].x = Math.random() * (800 - 0) + 0;
+		console.log("colliding");
+		p1Score++;
+	}
+
+	if(square[i].y == 800){
+		square[i].y = Math.random() * (100 - 0) + 0;
+		square[i].x = Math.random() * (800 - 0) + 0;
+	}
+
 
 
 //score hud
-	context.font = "30px Arial black";
+	context.font = "30px Arial";
 	context.fillStyle = "black";
 	context.textAlign = "center";
-	context.fillText("score: ", 80, 25); 
+	context.fillText("Score: " + p1Score, 80, 25); 
 
 
-	ball.drawCircle();
+	ball[i].drawCircle();
+	square[i].drawRect();
 	player.drawRect();
 }
 
